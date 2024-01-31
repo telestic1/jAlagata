2bb9551c65856f2c8707175376c4d6858ff4f5ee
lokesh <mrlokeshkumar56@gmail.com>
2024-01-31 11:04:37 +0530

    jAlagata regis & signin Activity

---
+++ app/src/main/java/com/example/jalagata/SignInAndRegistrationActivity.kt
@@ -0,0 +1,11 @@
+package com.example.jalagata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignInAndRegistrationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in_and_registration)
+    }
+}
