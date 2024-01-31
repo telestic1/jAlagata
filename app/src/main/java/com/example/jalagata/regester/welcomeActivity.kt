8135ad575b6d96f73b774baee4f5da1cba6381d9
lokesh <mrlokeshkumar56@gmail.com>
2024-01-31 12:39:37 +0530

    jAlagata welcome Activity

--- app/src/main/java/com/example/jalagata/regester/welcomeActivity.kt
+++ app/src/main/java/com/example/jalagata/regester/welcomeActivity.kt
@@ -1,12 +1,29 @@
 package com.example.jalagata.regester
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.jalagata.R
+import com.example.jalagata.SignInAndRegistrationActivity
+import com.example.jalagata.databinding.ActivityWelcomeBinding
 
 class welcomeActivity : AppCompatActivity() {
+   private  val binding:ActivityWelcomeBinding by lazy {
+       ActivityWelcomeBinding.inflate(layoutInflater)
+   }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(Intent(this,SignInAndRegistrationActivity::class.java))
+            intent.putExtra("action","login")
+            startActivity(intent)
+        }
+
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(Intent(this,SignInAndRegistrationActivity::class.java))
+            intent.putExtra("action","register")
+            startActivity(intent)
+        }
     }
 }
